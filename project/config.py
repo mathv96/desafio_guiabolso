bfe3d53e3bd32af2234de2d234c02124deb682b2
mathv96 <matheus.victor@ymail.com>
2019-03-31 23:22:31 -0300

    load the config variables.

---
+++ project/config.py
@@ -0,0 +1,7 @@
+import os
+
+user_mysql = os.environ['MYSQL_USER']
+password_mysql = os.environ['MYSQL_ROOT_PASSWORD']
+host = os.environ['MYSQL_HOST_DNS']
+database = os.environ['MYSQL_DATABASE']
+table_name = os.environ['MYSQL_TABLE_NAME']
