d7b78c727134849f95ab0d22da447b47291d1560
mathv96 <matheus.victor@ymail.com>
2019-04-03 23:23:04 -0300

    covertion to class.

--- project/DAO/insert.py
+++ project/DAO/insert.py
@@ -3,22 +3,23 @@ from datetime import datetime
 
 from config import * 
 
-def insert_event(event):
-    conn = connection.connection_mysql() 
-    query = "INSERT INTO {} (component, version, responsible, status, date) VALUES('{}', '{}', '{}', '{}', '{}')".format(
-            table_name, event.component, event.version, event.responsible, event.status, event.date
-        )
-    if conn:
-        cursor = conn.cursor()
-        try:
-            cursor.execute(query)
-            conn.commit()
-            conn.close()
-        except Exception as e:
-            conn.rollback()
-            conn.close()
-            print(e.__context__)
+class Insert():
+    def insert_event(self, event):
+        conn = connection.connection_mysql() 
+        query = "INSERT INTO {} (component, version, responsible, status, date) VALUES('{}', '{}', '{}', '{}', '{}')".format(
+                table_name, event.component, event.version, event.responsible, event.status, event.date
+            )
+        if conn:
+            cursor = conn.cursor()
+            try:
+                cursor.execute(query)
+                conn.commit()
+                conn.close()
+            except Exception as e:
+                conn.rollback()
+                conn.close()
+                print(e.__context__)
+                return False
+            return True
+        else:
             return False
-        return True
-    else:
-        return False
